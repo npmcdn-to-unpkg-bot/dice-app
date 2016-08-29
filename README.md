49eac25333fefda406f659bb044d8c71cc2a9d40
Gediminas Lisauskas <glisauskas@gmail.com>
2016-08-29 22:35:58 +0300

    Added simple tracking of connected sockets in an array

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # dice-app
 
+###Using docker:
 1. docker build -t glisauskas/dice-app .
 2. docker run -p 80:8080 -d glisauskas/dice-app
+
+###or not:
+1. npm install
+2. node app.js
---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+var io, socket_list, player_list;
+
+init();
+
+function init() {
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/client/index.html');
+    });
+    app.use('/client', express.static(__dirname + '/client'));
+    serv.listen(8080)
+
+    io = require('socket.io')(serv, {});
+    socket_list = [];
+    player_list = [];
+    setEventHandlers();
+}
+
+function setEventHandlers() {
+    io.sockets.on("connection", onSocketConnection);
+}
+
+function onSocketConnection(socket) {
+    onPlayerConnect(socket);
+    socket.on("disconnect", onPlayerDisconnect);
+}
+
+function onPlayerConnect(socket) {
+    console.log('Player connected: ' + socket.id);
+    socket_list.push(socket);
+    listConnectedSockets();
+}
+
+function onPlayerDisconnect() {
+    console.log('Player disconected' + this.id);
+    socket_list.splice(socket_list.indexOf(this), 1);
+    listConnectedSockets();
+}
+
+function listConnectedSockets() {
+    console.log('Number of sockets connected: ' + socket_list.length);
+    socket_list.forEach(function (socket) {
+        console.log(socket.id);
+    });
+}
